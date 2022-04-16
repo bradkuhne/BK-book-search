dee2ca82c288e135d89ba04e8c33226ac1fadebd
Brad Kuhne <bjkuhne@aol.com>
2022-04-16 15:44:04 -0500

    updated missing data defnition in loginform.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
 import {useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -29,7 +28,7 @@ const LoginForm = () => {
     }
 
     try {
-      
+      const { data } = await loginUser({ variables: { ...userFormData }});
       if (!data) {
         throw new Error('something went wrong!');
       }
